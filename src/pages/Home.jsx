6e72f5b219679676f1441777bfdb1aaf1fa15e0a
abyss-s <xyz@inha.edu>
2024-05-20 19:29:13 +0900

    feat: 2번

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 
 const Home = () => {
-    return (
-        <> 
-            <h1>Main Page</h1>
-            <Link to="/menu">메뉴 페이지로 고고</Link>
-        </>
-    );
+  const [userInfo, setUserInfo] = useState({
+    avatar: "",
+    eamil: "",
+    first_name: "",
+    id: 0,
+    last_name: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get(`https://reqres.in/api/users/${1}`)
+      .then((res) => {
+        setUserInfo(res.data.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Main Page</h1>
+      <img src={userInfo.avatar} />
+      <h3>email: {userInfo.email}</h3>
+      <h3>{userInfo.first_name} {userInfo.last_name}</h3>
+      <Link to="/menu">메뉴 페이지로 고고</Link>
+    </>
+  );
 };
 
 export default Home;
-
