777e0ac53819db4f8ca9de9752eb0c3b17b229b4
abyss-s <xyz@inha.edu>
2024-05-27 17:36:55 +0900

    design: 레이아웃 및 반응형

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import GoToArticle from "./pages/GoToArticle";
 import NotFound from "./pages/NotFound";
+import Layout from './components/Layout/Layout';
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/article/:articleId" element={<GoToArticle />} />
         <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
